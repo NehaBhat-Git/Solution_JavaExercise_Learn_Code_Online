dc4c01b4fefdef51ebb71abaa82aaa61eea3bc1f
NehaBhat-Git <89778970+NehaBhat-Git@users.noreply.github.com>
2022-10-18 12:48:13 +0530

    Update Hello.java

--- Hello_World/src/Hello.java
+++ Hello_World/src/Hello.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        System.out.println("Hello world!"); //this code line will print output
         /* What is Variable?
         Variable is a container which store the data values while java
         program is executed*/
